bf38ee3ae2c57beb0e0d515d57c20c328cc3e8ff
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-05-29 19:23:40 -0300

    testando commit no eclipse

--- src/com/projetoEstudar/service/ExameService.java
+++ src/com/projetoEstudar/service/ExameService.java
@@ -4,5 +4,6 @@ import com.projetoEstudar.model.Exame;
 
 public class ExameService {
 	void salvarExame(Exame exame) {
-		System.out.println("Hello Word");}
+		System.out.println("Hello Word");
+		System.out.println("Testando commit do eclipse");	}
 }
