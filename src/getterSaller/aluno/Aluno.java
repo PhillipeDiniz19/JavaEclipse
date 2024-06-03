51d90796477800c18340027ddb2ac6da354807ef
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-06-03 16:16:45 -0300

    usando atalhos da IDE

--- src/getterSaller/aluno/Aluno.java
+++ src/getterSaller/aluno/Aluno.java
@@ -3,6 +3,15 @@ package getterSaller.aluno;
 public class Aluno {
 	private String nome;
 	private int idade;
+	private String sexo;
+	
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
+	public String getSexo() {
+		return sexo;
+	}
 	
 	public String getNome() {
 		return nome;
