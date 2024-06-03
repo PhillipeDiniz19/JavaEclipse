23c0555bf16ed08a1aca0741649ec36321d5f311
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-06-03 16:08:48 -0300

    atribuindo metodos de get e set

--- src/getterSaller/aluno/Aluno.java
+++ src/getterSaller/aluno/Aluno.java
@@ -1,6 +1,18 @@
-package getterSaller.aluno;
-
+//arquivo Aluno.java
 public class Aluno {
-	String nome;
-	int idade;
+	private String nome;
+	private int idade;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String newNome) {
+		nome = newNome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int newIdade) {
+		this.idade = newIdade;
+	}
 }
